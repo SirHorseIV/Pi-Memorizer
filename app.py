a73022552ed070d09949d7a6a0e2affe111f245e
SirHorseIV <78271552+SirHorseIV@users.noreply.github.com>
2022-03-10 20:16:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -19,31 +19,59 @@ text=font.render(digits[:index],True,(255,255,255))
 textRect=text.get_rect(center=(WINDOW_SIZE[0]/2,WINDOW_SIZE[1]/2))
 smallText=smallFont.render(str(digitNum),True,(255,255,255))
 cam=camera(textRect.midright,10,WINDOW_SIZE)
+playing=True
 
 # Game Loop
 running=True
 while running:
-    cam.update(textRect.midright)
-    # Draw
-    screen.fill((40,43,48))
-    screen.blit(text,textRect.move(cam.smoothScroll).move(WINDOW_SIZE[0]*2/5,10).topleft)
-    screen.blit(smallText,(20,0))
-    # Update Display
-    pygame.display.update()
-    clock.tick(FPS)
     # Get Input
     inp.getInput()
-    # Handle Input
     if inp.quit:
         pygame.quit()
         running=False
-    inp.getStringInput()
-    character=inp.character
-    if character!="":
-        if character==digits[index]:
-            index+=1
-            if digits[index-1]!=".":
-                digitNum+=1
+    if playing:
+        cam.update((textRect.move(-WINDOW_SIZE[0]*2/5,-10).midright))
+        # Draw
+        screen.fill((40,43,48))
+        screen.blit(text,textRect.move(cam.smoothScroll).topleft)
+        screen.blit(smallText,(20,0))
+        # Handle Input
+        inp.getStringInput()
+        character=inp.character
+        if character!="":
+            if character==digits[index]:
+                index+=1
+                if digits[index-1]!=".":
+                    digitNum+=1
+                text=font.render(digits[:index],True,(255,255,255))
+                textRect=text.get_rect(center=(WINDOW_SIZE[0]/2,WINDOW_SIZE[1]/2))
+                smallText=smallFont.render(str(digitNum),True,(255,255,255))
+            else:
+                wrong=True
+                playing=False
+    elif wrong:
+        cam.update(textRect.move(WINDOW_SIZE[0]*1/4,-10).midright)
+        # Draw
+        screen.fill((202,52,51))
+        text=font.render(digits[:index],True,(255,255,255))
+        textRect=text.get_rect(center=(WINDOW_SIZE[0]/2,WINDOW_SIZE[1]/2))
+        screen.blit(text,textRect.move(cam.smoothScroll).topleft)
+        nextText=font.render(digits[index:index+10],True,(245,113,139))
+        nextTextRect=nextText.get_rect(midleft=textRect.midright)
+        screen.blit(nextText,nextTextRect.move(cam.smoothScroll).topleft)
+        smallText=smallFont.render("Press R to reset",True,(255,255,255))
+        screen.blit(smallText,(20,0))
+        # Handle Input
+        inp.getStringInput()
+        character=inp.character
+        if character=="r":
+            index=0
+            digitNum=0
             text=font.render(digits[:index],True,(255,255,255))
-            textRect=text.get_rect(center=(WINDOW_SIZE[0]/2,WINDOW_SIZE[1]/2))
             smallText=smallFont.render(str(digitNum),True,(255,255,255))
+            playing=True
+            wrong=False
+
+    # Update Display
+    pygame.display.update()
+    clock.tick(FPS)
